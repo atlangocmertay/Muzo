17f29f99bcc51100a45205eadb58805c6ef951ca
Mert <74057785+atlangocmertay@users.noreply.github.com>
2020-11-10 01:04:17 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,18 @@
+#Exercise 4.1 a
+list1 = [5, 10, "twenty"]
+list1.remove(10)
+print(list1)
+
+#Exercise 4.1 b
+tuple1 = (5, 10, "twenty")
+tuple1.remove(10)
+print(tuple1)
+#It doesn't work because we can't remove items in a tuple.
+
+#Exercise 4.1 c
+set1 = {5, 10, "twenty"}
+set2 = {"twenty", 10, 5}
+print(set1, set2)
+
+#Exercise 4.1 d
+days = {1: "Monday",2: "Tuesday",3: "Wednesday",4: "Thursday",5: "Friday",6: "Saturday",7: "Sunday"}
